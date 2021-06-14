cebcd4f18a9a7043ff06517398d94f5e8eebf31f
ext.joao.g.monteiro <ext.joao.g.monteiro@farfetch.com>
2021-06-14 21:32:11 +0100

    implement styles recipe page

--- src/pages/Recipe/styles.js
+++ src/pages/Recipe/styles.js
@@ -3,23 +3,62 @@ import styled from "styled-components";
 export const RecipeContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-  width: 35rem;
+  min-height: calc(100vh - 5rem - 9rem);
   margin: 0 auto;
+  margin-top: 2.5rem;
 `;
 
 export const Header = styled.header`
-  width: 35rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50rem;
 
+  .underline {
+    height: 0.2rem;
+    width: 100%;
+    background: #fff;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .p {
+    font-size: 1.2rem;
+  }
+`;
+
+export const RecipeMainContent = styled.main`
+  margin-top: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .recipe__image {
+    width: 55rem;
+  }
   img {
     width: 100%;
   }
 `;
 
-export const Ingredients = styled.div``;
+export const RecipePreparation = styled.main`
+  font-size: 1.2rem;
+  margin-left: 5rem;
+  max-width: 30vw;
+`;
+
+export const Ingredients = styled.div`
+  li:not(:first-child) {
+    margin-top: 0.5rem;
+  }
+`;
 
 export const Steps = styled.div`
-  & > li:not(:first-child) {
-    margin-top: 1rem;
+  li:not(:first-child) {
+    margin-top: 0.5rem;
   }
 `;
